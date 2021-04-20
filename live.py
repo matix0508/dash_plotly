acd79622430d1e08728a11b31c074671bc4669eb
matix0508 <matix0508@gmail.com>
2021-04-20 16:29:28 +0200

    live init

--- live.py
+++ live.py
@@ -1,2 +1,24 @@
 import dash
 from dash.dependencies import Output, Event
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+
+X = deque(maxlen=20)
+Y = deque(maxlen=20)
+X.append(1)
+Y.append(1)
+
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    [
+        dcc.Graph(id='livegraph', animate=True),
+        dcc.Interval(
+            id='graph-update',
+            interval=1000
+        )
+    ]
+)
